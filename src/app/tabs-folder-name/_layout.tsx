969447c29a82f9f2d3d6056d09dd248f2a026b47
VictorSarov <victor.sarov@rentalmatics.com>
2026-09-23 08:20:28 +0100

    all good

--- src/app/tabs-folder-name/_layout.tsx
+++ src/app/tabs-folder-name/_layout.tsx
@@ -1,5 +1,11 @@
 import { Tabs } from 'expo-router';
 
 export default function TabsFolderNameLayout() {
-    return <Tabs></Tabs>;
+    return (
+        <Tabs
+            screenOptions={{
+                headerShown: false
+            }}
+        ></Tabs>
+    );
 }
