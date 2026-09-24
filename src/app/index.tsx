436a9ef39193a748bf1f3d6d9373996e487ce9e7
VictorSarov <victor.sarov@rentalmatics.com>
2026-09-24 11:46:45 +0100

    all good

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,14 @@
-import { useState } from 'react';
-import { Alert, Button, Text, TextInput } from 'react-native';
+import * as Clipboard from 'expo-clipboard';
+import * as Haptics from 'expo-haptics';
+import { router, useFocusEffect } from 'expo-router';
+import { useCallback, useEffect, useState } from 'react';
+import { Alert, Button, Pressable, Share, Text, TextInput, TouchableOpacity, View } from 'react-native';
 
 export default function Index() {
     const [text, setText] = useState('');
-    console.log('alert object', Alert);
+    console.log('Clipboard', Clipboard);
+    // console.log('Haptics object', Haptics);
+    // console.log('alert object', Alert);
 
     // useEffect(() => {
     //     const loadStorage = async () => {
@@ -19,6 +24,27 @@ export default function Index() {
     //     void loadStorage();
     // }, []);
 
+    useFocusEffect(
+        useCallback(() => {
+            console.log('Screen is focused');
+            return () => {
+                console.log('Screen is unfocused');
+            };
+        }, [])
+    );
+
+    useEffect(() => {
+        console.log('mounted');
+
+        return () => {
+            console.log('unmounted');
+        };
+    }, []);
+
+    const hapticsFn = () => {
+        Haptics.notificationAsync(Haptics.NotificationFeedbackType.Success);
+    };
+
     return (
         <>
             <Text>Hello</Text>
@@ -30,10 +56,80 @@ export default function Index() {
             <Button
                 title='Show Alert'
                 onPress={() => {
-                    console.log('alert triggered');
                     Alert.alert('Alert', `You entered: ${text}`);
                 }}
             />
+            <Button title='go to about' onPress={() => router.push('/about')} />
+            <Pressable onLongPress={() => console.log('long press pressable')}>
+                <Text>Long press pressable</Text>
+            </Pressable>
+            <TouchableOpacity
+                onLongPress={() => {
+                    console.log('long press touchable');
+                    Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Heavy);
+                }}
+            >
+                <Text>Long press touchable</Text>
+            </TouchableOpacity>
+            <View
+                style={{
+                    height: 1,
+                    backgroundColor: '#ccc',
+                    width: '100%'
+                }}
+            />
+            <Button
+                title='vibrate'
+                onPress={() => {
+                    Haptics.notificationAsync(Haptics.NotificationFeedbackType.Success);
+                    Share.share({
+                        message: 'Check out this cool app!'
+                    });
+                }}
+            />
+
+            <View
+                style={{
+                    height: 1,
+                    backgroundColor: '#ccc',
+                    width: '100%',
+                    marginBottom: 5
+                }}
+            />
+            <Button
+                title='share'
+                onPress={() => {
+                    Share.share({
+                        message: 'Check out this cool app!'
+                    });
+                }}
+            />
+
+            <View
+                style={{
+                    height: 1,
+                    backgroundColor: '#ccc',
+                    width: '100%',
+                    marginBottom: 5
+                }}
+            />
+            <Button
+                title='clipboard'
+                onPress={() => {
+                    Clipboard.setStringAsync('Hello from Expo!');
+                }}
+            />
+
+            <View
+                style={{
+                    height: 1,
+                    backgroundColor: '#ccc',
+                    width: '100%',
+                    marginBottom: 5
+                }}
+            />
+
+            {/* <ReminderToggle /> */}
         </>
     );
 }
